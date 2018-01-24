34313ca6f99aa22359e2d10d0ad42bd268e7a987
varunkishoreravi <35689715+varunkishoreravi@users.noreply.github.com>
2018-01-24 17:42:15 +0530

    initial commit

---
+++ my_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\operam_007\varun\first-django-project\my_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
